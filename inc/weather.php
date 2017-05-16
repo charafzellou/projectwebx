9524a59c583221cfc4f7a3c58cb808fcf01ebb0a
lcolat <loic.colat@gmail.com>
2017-05-16 13:46:30 +0000

    modification de la fonction d'affichage du temps 	modified:   inc/weather.php

--- inc/weather.php
+++ inc/weather.php
@@ -5,7 +5,7 @@ try
 {
 
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=aen;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=aen;charset=utf8', 'root', 'aenprojectx');
 
 }
 
@@ -30,56 +30,11 @@ while ($donnees = $reponse->fetch())
 
 {
 
-
-
     if( $donnees['weather'] == 800)
     {
     ?>
         <i class="wi wi-day-sunny"></i>Ensoleillé
     <?php
-    }
-    elseif($donnees['weather'] >= 200 && $donnees['weather'] <= 232)
-    {
-        ?>
-        <i class="wi wi-day-thunderstorm">Tempête</i>
-        <?php
-    }
-    elseif($donnees['weather'] >= 300 && $donnees['weather'] <= 321)
-    {
-        ?>
-        <i class="wo wi-day-rain-mix">Bruine</i>
-        <?php
-    }
-    elseif($donnees['weather'] >= 500 && $donnees['weather'] <= 504)
-    {
-        ?>
-        <i class="wi wi-day-rain">Bruine</i>
-        <?php
-    }
-    elseif($donnees['weather'] == 511)
-    {
-        ?>
-        <i class="wi wi-snow">Pluie verglassante</i>
-        <?php
-    }
-    elseif($donnees['weather'] >= 520 && $donnees['weather'] <= 531)
-    {
-        ?>
-        <i class="wi wi-rain">Pluie</i>
-        <?php
-    }
-    elseif($donnees['weather'] >= 600 && $donnees['weather'] <= 622)
-    {
-        ?>
-        <i class="wi wi-snow">Neige</i>
-        <?php
-    }
-    
-    elseif($donnees['weather'] >= 701 && $donnees['weather'] <= 781)
-    {
-        ?>
-        <i class="wi wi-dust"></i> Brouillard
-        <?php
     }
     
     echo $donnees['tempnow'] - 273.15,'°C';
